8d41a442040eefe85ce927d8045fdead8b9efaab
Lorenzo Martinico <lzmartinico@gmail.com>
2016-02-11 20:24:37 +0000

    Add Voronoi distance and default files

--- src/Planner.java
+++ src/Planner.java
@@ -5,9 +5,12 @@ public class Planner {
 	int height;
 	Point2D[] warehouses;
     //1. Divide map into voronoi regions
+	//each cell in map contains the index of the closest warehouse
 	int[][] map = Voronoi.buildVoronoiMap(warehouses, width, height);
     //2. Sort areas by number of orders that need to be delivered there
+	//
     //3. Send drones to warehouse in order (one to each or as many as necessary?)
+	
     //4. Each drone will load as many products as it can for houses that are close by each other
     // (but we prefer a single drone to fill out one order)
     //5. Deliver to the house in order of distance from warehouse
